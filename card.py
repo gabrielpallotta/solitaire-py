3f6127b42951faa7512d85b1556b19555b197d33
gabrielpallotta <gabriel.cardenete@hotmail.com>
2018-11-01 20:47:51 -0300

    Create separate class to represent a Card

--- card.py
+++ card.py
@@ -2,6 +2,8 @@ import pygame
 from pygame.locals import *
 from pygame.sprite import Sprite
 
+from spritesheet import SpriteSheet
+
 from enum import Enum
 
 class CardSuit(Enum):
@@ -10,27 +12,50 @@ class CardSuit(Enum):
     HEARTS = 3
     CLUBS = 4
 
-class Card(Sprite):
-    def __init__(self, image, pos = (0, 0)):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = image
+class Card:
+    def __init__(self, suit, value, visible):
+        self.suit = suit
+        self.value = value
+        self.visible = visible
+
+class CardSprite(Sprite):
+    def __init__(self, card, pos = (0, 0)):
+        Sprite.__init__(self)
+        self.card = card
+        self.image = CardSprite.get_card_image(self.card)
         self.rect = Rect(pos[0], pos[1], self.image.get_width(), self.image.get_height())
         self.moving = False
         self.last = pos
 
-    def mouse_move(self, new_mouse_pos):
-        if self.moving:
-            self.rect.move_ip((new_mouse_pos[0] - self.last[0], new_mouse_pos[1] - self.last[1]))
-            self.last = new_mouse_pos
+    @staticmethod
+    def load_spritesheet():
+        CardSprite.cards_spritesheet = SpriteSheet("res/cards_sprite.gif", 13, 5)
 
-    def start_drag(self, mouse_pos):
-        if self.rect.collidepoint(mouse_pos):
-            self.last = mouse_pos
-            self.moving = True
-            return True
-        else:
-            return False
+    @staticmethod
+    def get_card_image(card):
+        if not card.visible:
+            return CardSprite.cards_spritesheet.get_image(0, 4)
 
-    def end_drag(self):
-        self.moving = False
+        suit = card.suit
+        value = card.value
+
+        value -= 2
+        if value < 0:
+            value = 12
+
+        if suit == CardSuit.DIAMONDS:
+            suit = 1
+        elif suit == CardSuit.SPADES:
+            suit = 3
+        elif suit == CardSuit.HEARTS:
+            suit = 0
+        elif suit == CardSuit.CLUBS:
+            suit = 2
+        
+        return CardSprite.cards_spritesheet.get_image(value, suit)
+    
+    def update_card(card):
+        self.card = card
+        self.image = CardSprite.get_card_image(self.card)
+
 
