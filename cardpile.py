4469b130deab5916889aad0502081a6c0ba9aef8
gabrielpallotta <gabriel.cardenete@hotmail.com>
2018-11-01 21:46:39 -0300

    Implement drag and drop between tableaus

--- cardpile.py
+++ cardpile.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from pygame.sprite import Group
-from card import CardSprite
+from card import Card
 
 class CardPile(Group):
     def __init__(self, pos):
@@ -8,11 +8,53 @@ class CardPile(Group):
         self.pos = pos
     
     def add_card(self, card):
-        Group.add(self, CardSprite(card, self.pos))
+        Group.add(self, card)
 
 class TableauPile(CardPile):
     def __init__(self, pos):
         CardPile.__init__(self, pos)
+
+    def add_card(self, card):
+        card.rect.x = self.pos[0]
+        card.rect.y = self.pos[1] + 50 * len(self.sprites())
+        # if not self.cards:
+        #     if not visible:
+        Group.add(self, card)
+
+    def drag(self, mouse_pos):
+        card_start = None
+
+        for sprite in reversed(self.sprites()):
+            if sprite.rect.collidepoint(mouse_pos) and sprite.visible:
+                card_start = sprite
+                break
+        
+        card_drag_list = []
+        if card_start:
+            card_drag_list = self.sprites()[self.sprites().index(card_start):]
+            del self.sprites()[self.sprites().index(card_start):]
+
+        for card_sprite in card_drag_list:
+            self.remove(card_sprite)
+
+        return card_drag_list
+
+    def drop(self, mouse_pos, card_sprite_list):
+        can_drop = False
+        for sprite in self.sprites():
+            if sprite.rect.collidepoint(mouse_pos):
+                can_drop = True
+                break
+
+        if can_drop:
+            # check stuff
+
+            for card in card_sprite_list:
+                self.add_card(card)
+
+            return True
+        else:
+            return False
 
 class FoundationPile(CardPile):
     def __init__(self, pos):
--- solitaire.py
+++ solitaire.py
@@ -3,6 +3,8 @@ from pygame.locals import *
 from pygame.sprite import Group
 from pygame import Surface
 
+from random import shuffle
+
 from enum import Enum
 from card import Card, CardSuit
 from cardpile import *
@@ -21,43 +23,57 @@ class Solitaire:
         self.screen = pygame.display.set_mode((800, 600))
         pygame.display.set_caption("Solitaire")
 
-        CardSprite.load_spritesheet()
-
-        self.dragged_cards = None
+        Card.load_spritesheet()
+        
+        self.dragged_cards_pile = None
+        self.dragged_cards = []
 
         self.background = Surface(self.screen.get_size())
         self.background = self.background.convert()
         self.background.fill((68, 163, 92))
 
         self.cards = []
-        for i in range(1, 5):
-            for j in range(1, 14):
-                self.cards.insert(len(self.cards), Card(i, j, False))
+        for i in range(0, 4):
+            for j in range(0, 13):
+                self.cards.append((Card(i, j, True)))
 
         # Card piles
         self.stock = StockPile((0, 0))
         self.tableaus = []
         for i in range(0, 7):
-            self.tableaus.insert(len(self.tableaus), TableauPile((100 * i, 50)))
+            self.tableaus.append(TableauPile((100 * i, 50)))
             for j in range (0, i + 1):
-                self.tableaus[i].add_card(self.cards[10])
+                self.tableaus[i].add_card(self.cards[7 * i + j])
 
-        for i in range(0, 50):
-            self.stock.add_card(self.cards[10])
+        # for i in range(0, 50):
+        #     self.stock.add_card(self.cards[10])
 
     def update(self):
+        print(self.dragged_cards)
         for event in pygame.event.get():
             if event.type == QUIT:
                 self.should_quit = True
             elif event.type == MOUSEBUTTONDOWN:
-                for tableaus in self.tableaus:
-                    if sprite.start_drag(event.pos):
-                        self.dragged_card = sprite
+                for tableau in self.tableaus:
+                    self.dragged_cards = tableau.drag(event.pos)
+                    if self.dragged_cards:
+                        self.dragged_cards_pile = tableau
                         break
             elif event.type == MOUSEMOTION:
+                mouse_rel = pygame.mouse.get_rel()
+                for sprite in self.dragged_cards:
+                    sprite.move(mouse_rel)
                 
             elif event.type == MOUSEBUTTONUP:
-                
+                if self.dragged_cards:
+                    for tableau in self.tableaus:
+                        if tableau.drop(event.pos, self.dragged_cards):
+                            self.dragged_cards = []
+                            break
+                    for card in self.dragged_cards:
+                        self.dragged_cards_pile.add_card(card)
+                    self.dragged_cards = []
+                    
         self.stock.update()
         for tableau in self.tableaus:
             tableau.update()
@@ -65,7 +81,11 @@ class Solitaire:
     def render(self):
         self.screen.blit(self.background, (0, 0))
         self.stock.draw(self.screen)
+
         for tableau in self.tableaus:
             tableau.draw(self.screen)
 
+        for card_sprite in self.dragged_cards:
+            self.screen.blit(card_sprite.image, card_sprite.rect)
+        
         pygame.display.flip()
